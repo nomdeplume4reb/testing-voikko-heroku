55019aba5409f57d2081ff5f7d331422a4129909
nomdeplume4reb <48643044+nomdeplume4reb@users.noreply.github.com>
2021-09-04 13:48:12 +0300

    path

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import libvoikko
 from libvoikko import Voikko
 # v = libvoikko.Voikko(u"fi")
 
-Voikko.setLibrarySearchPath("/app/Voikko")
-v = libvoikko.Voikko("fi", "/app/Voikko/dict")
+#Voikko.setLibrarySearchPath("/app/Voikko")
+#v = libvoikko.Voikko("fi", "/app/Voikko/dict")
 
 app = Flask(__name__)
 
@@ -17,7 +17,8 @@ def practice():
 @app.route("/")
 @app.route("/home", methods=["POST", "GET"])
 def home():
-    test = practice()
+    #test = practice()
+    test='test'
     return render_template('home.html', test=test)
 
 if __name__ == '__main__':
