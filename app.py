c10b0cefc0b3dfca877e32fa0b0460d197a47938
nomdeplume4reb <48643044+nomdeplume4reb@users.noreply.github.com>
2021-09-04 13:30:18 +0300

    fiel path

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, redirect
+import os
 
 import libvoikko
 from libvoikko import Voikko
 # v = libvoikko.Voikko(u"fi")
-Voikko.setLibrarySearchPath(r"Voikko")
-v = libvoikko.Voikko("fi", r"Voikko\dict")
+
+test = os.path.abspath(__file__)
+
+#Voikko.setLibrarySearchPath(r"Voikko")
+#v = libvoikko.Voikko("fi", r"Voikko\dict")
 
 app = Flask(__name__)
 
@@ -15,7 +19,7 @@ def practice():
 @app.route("/")
 @app.route("/home", methods=["POST", "GET"])
 def home():
-    test = practice()
+    #test = practice()
     return render_template('home.html', test=test)
 
 if __name__ == '__main__':
